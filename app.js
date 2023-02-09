50074d9af8cddf3d90c5e06bfb7290b8b070413f
daedalus <daedalus@debian-BULLSEYE-live-builder-AMD64>
2023-02-08 20:20:49 -0800

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const stress_dict = JSON.parse(fs.readFileSync('private/data/compiled.json', 'ut
 
 let subdomain = '/poetry';
 
-app.use(express.static(`${subdomain}${__dirname}/public`, { extensions: ['html'] })); // tells the app to use a public folder and remove the html extension
+app.use(express.static(`${subdomain}/${__dirname}/public`, { extensions: ['html'] })); // tells the app to use a public folder and remove the html extension
 
 app.get(`${subdomain}/`, (req, res) => {
 
