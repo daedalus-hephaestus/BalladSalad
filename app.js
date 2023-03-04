016a00bb5c2d3b1a8b1790bf41d1497f3fb52412
daedalus <daedalus@debian-BULLSEYE-live-builder-AMD64>
2023-03-03 18:14:08 -0800

    removed console.log

--- app.js
+++ app.js
@@ -210,9 +210,7 @@ app.post('/forgot', async (req, res) => {
         user = await User.findOne({ email_case: username });
 
     }
-
-    console.log(user);
-
+    
     if (!user) { // if no user is found
 
         return res.redirect('/login'); // redirec to the login page
